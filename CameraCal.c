8d5ad3ed068ece7cc96cf5b4f59e40dd020918d5
Philip Pemberton <philpem@philpem.me.uk>
2012-08-04 19:42:07 +0100

    clean up CameraCal fcns

--- CameraCal.c
+++ CameraCal.c
@@ -23,14 +23,13 @@ int sub_10003400(int a1, int a2, signed int a3)
 	if (((a3 / 2) + v3) > 40)
 		return 5;
 
-	if (((a3 / 2) + v5) <= 40)
-		return 0;
+	if (((a3 / 2) + v5) > 40)
+		return 6;
 
-	return 6;
-//	return ((v5 + a3 / 2 <= 40) - 1) & 6;
+	return 0;
 }
 
-void sub_10003730(signed int a1, signed int a2, signed int a3, signed int a4, int *a5, int *a6, signed int *a7)
+void CameraCalibrate(signed int x1_yellow, signed int y1_magenta, signed int x2_cyan, signed int y2_green, int *cal_x, int *cal_y, signed int *cal_z)
 {
 	double v8; // st7@2
 	double v9; // st6@2
@@ -44,15 +43,15 @@ void sub_10003730(signed int a1, signed int a2, signed int a3, signed int a4, in
 	double v17; // st7@16
 	int v19; // eax@19
 
-	if ( a4 + a2 <= a3 + a1 )
+	if ( y2_green + y1_magenta <= x2_cyan + x1_yellow )
 	{
-		v8 = 40.0 - (double)a1;
-		v9 = (double)a3;
+		v8 = 40.0 - (double)x1_yellow;
+		v9 = (double)x2_cyan;
 	}
 	else
 	{
-		v8 = 40.0 - (double)a2;
-		v9 = (double)a4;
+		v8 = 40.0 - (double)y1_magenta;
+		v9 = (double)y2_green;
 	}
 
 	v10 = 80.0 - (v8 + 40.0 - v9) * 3.0 * 1.098901098901099;
@@ -62,50 +61,50 @@ void sub_10003730(signed int a1, signed int a2, signed int a3, signed int a4, in
 	else
 		v11 = v10 + 0.5;
 
-	*a7 = (signed int)v11;
+	*cal_z = (signed int)v11;
 
 	if (sub_10003400(0, 0, (signed int)v11))
 	{
 		do
 		{
-			v12 = *a7 - ((((*a7 <= 0) - 1) & 2) - 1);
-			*a7 = v12;
+			v12 = *cal_z - ((((*cal_z <= 0) - 1) & 2) - 1);
+			*cal_z = v12;
 		}
 		while ( sub_10003400(0, 0, v12) );
 	}
-	v13 = ((double)*a7 * 0.001 + 1.0) * 0.9259259259259258 * (double)(a3 - a1) * 3.0 * 0.5;
+	v13 = ((double)*cal_z * 0.001 + 1.0) * 0.9259259259259258 * (double)(x2_cyan - x1_yellow) * 3.0 * 0.5;
 	if ( v13 < 0.0 )
 		v14 = v13 - 0.5;
 	else
 		v14 = v13 + 0.5;
-	*a5 = (int)v14;
-	if ( sub_10003400((int)v14, 0, *a7) )
+	*cal_x = (int)v14;
+	if ( sub_10003400((int)v14, 0, *cal_z) )
 	{
 		do
 		{
-			v15 = *a5 - ((((*a5 <= 0) - 1) & 2) - 1);
-			*a5 = v15;
+			v15 = *cal_x - ((((*cal_x <= 0) - 1) & 2) - 1);
+			*cal_x = v15;
 		}
-		while ( sub_10003400(v15, 0, *a7) );
+		while ( sub_10003400(v15, 0, *cal_z) );
 	}
-	v16 = ((double)*a7 * 0.001 + 1.0) * 0.9259259259259258 * (double)(a4 - a2) * 3.0 * 0.5;
+	v16 = ((double)*cal_z * 0.001 + 1.0) * 0.9259259259259258 * (double)(y2_green - y1_magenta) * 3.0 * 0.5;
 	if ( v16 < 0.0 )
 		v17 = v16 - 0.5;
 	else
 		v17 = v16 + 0.5;
-	*a6 = (int)v17;
+	*cal_y = (int)v17;
 
-	if (sub_10003400(*a5, (int)v17, *a7)) {
+	if (sub_10003400(*cal_x, (int)v17, *cal_z)) {
 		do {
-			v19 = *a6 - ((((*a6 <= 0) - 1) & 2) - 1);
-			*a6 = v19;
-		} while (sub_10003400(*a5, v19, *a7));
+			v19 = *cal_y - ((((*cal_y <= 0) - 1) & 2) - 1);
+			*cal_y = v19;
+		} while (sub_10003400(*cal_x, v19, *cal_z));
 	}
 }
 
 int main(void)
 {
-	//sub_10003730(signed int a1, signed int a2, signed int a3, signed int a4, int *a5, int *a6, signed int *a7)
+	//CameraCalibrate(signed int a1, signed int a2, signed int a3, signed int a4, int *a5, int *a6, signed int *a7)
 
 	char vals[][4] = {
 		// a   b   c   d
@@ -132,7 +131,7 @@ int main(void)
 
 	for (i=40; i>=0; i--) {
 		int x, y, z;
-		sub_10003730(i,i,i,i, &x, &y, &z);
+		CameraCalibrate(i,i,i,i, &x, &y, &z);
 		printf("%2d %2d %2d %2d => %4d %4d %4d\n", i, i, i, i, x, y, z);
 	}
 
@@ -154,7 +153,7 @@ int main(void)
 		int a, b, c, d;
 		int x, y, z;
 		a = vals[i][0]; b = vals[i][1]; c = vals[i][2]; d = vals[i][3];
-		sub_10003730(a, b, c, d, &x, &y, &z);
+		CameraCalibrate(a, b, c, d, &x, &y, &z);
 		printf("%2d %2d %2d %2d => %4d %4d %4d\n", a, b, c, d, x, y, z);
 	}
 
