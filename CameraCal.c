1241cac1c89b0a2ae8dbda84fa8a50e75a71670f
Philip Pemberton <philpem@philpem.me.uk>
2012-08-04 20:52:56 +0100

    Simplify the code a little more

--- CameraCal.c
+++ CameraCal.c
@@ -3,15 +3,10 @@
 
 int sub_10003400(int a1, int a2, signed int a3)
 {
-	signed int v3; // ecx@1
-	signed int v5; // esi@3
-
-	v3 = abs(a1);
-	if (v3 > 40)
+	if (abs(a1) > 40)
 		return 1;
 
-	v5 = abs(a2);
-	if (v5 > 40)
+	if (abs(a2) > 40)
 		return 2;
 
 	if (a3 > 80)
@@ -20,10 +15,10 @@ int sub_10003400(int a1, int a2, signed int a3)
 	if (a3 < -50)
 		return 4;
 
-	if (((a3 / 2) + v3) > 40)
+	if (((a3 / 2) + abs(a1)) > 40)
 		return 5;
 
-	if (((a3 / 2) + v5) > 40)
+	if (((a3 / 2) + abs(a2)) > 40)
 		return 6;
 
 	return 0;
@@ -33,72 +28,48 @@ void CameraCalibrate(signed int x1_yellow, signed int y1_magenta, signed int x2_
 {
 	double v8; // st7@2
 	double v9; // st6@2
-	double v10; // st7@4
 	double v11; // st7@5
-	signed int v12; // eax@8
 	double v13; // st7@9
-	double v14; // st7@10
-	int v15; // eax@13
 	double v16; // st7@15
-	double v17; // st7@16
-	int v19; // eax@19
 
-	if ( y2_green + y1_magenta <= x2_cyan + x1_yellow )
-	{
+	if ((y2_green + y1_magenta) <= (x2_cyan + x1_yellow)) {
 		v8 = 40.0 - (double)x1_yellow;
 		v9 = (double)x2_cyan;
-	}
-	else
-	{
+	} else {
 		v8 = 40.0 - (double)y1_magenta;
 		v9 = (double)y2_green;
 	}
 
-	v10 = 80.0 - (v8 + 40.0 - v9) * 3.0 * 1.098901098901099;
-
-	if ( v10 < 0.0 )
-		v11 = v10 - 0.5;
-	else
-		v11 = v10 + 0.5;
-
+	v11 = 80.0 - (v8 + 40.0 - v9) * 3.0 * 1.098901098901099;
+	v11 = (v11 < 0.0) ? v11 - 0.5 : v11 + 0.5;
 	*cal_z = (signed int)v11;
 
-	if (sub_10003400(0, 0, (signed int)v11))
-	{
-		do
-		{
-			v12 = *cal_z - ((((*cal_z <= 0) - 1) & 2) - 1);
-			*cal_z = v12;
-		}
-		while ( sub_10003400(0, 0, v12) );
-	}
-	v13 = ((double)*cal_z * 0.001 + 1.0) * 0.9259259259259258 * (double)(x2_cyan - x1_yellow) * 3.0 * 0.5;
-	if ( v13 < 0.0 )
-		v14 = v13 - 0.5;
-	else
-		v14 = v13 + 0.5;
-	*cal_x = (int)v14;
-	if ( sub_10003400((int)v14, 0, *cal_z) )
-	{
-		do
-		{
-			v15 = *cal_x - ((((*cal_x <= 0) - 1) & 2) - 1);
-			*cal_x = v15;
-		}
-		while ( sub_10003400(v15, 0, *cal_z) );
-	}
-	v16 = ((double)*cal_z * 0.001 + 1.0) * 0.9259259259259258 * (double)(y2_green - y1_magenta) * 3.0 * 0.5;
-	if ( v16 < 0.0 )
-		v17 = v16 - 0.5;
-	else
-		v17 = v16 + 0.5;
-	*cal_y = (int)v17;
-
-	if (sub_10003400(*cal_x, (int)v17, *cal_z)) {
+	if (sub_10003400(0, 0, *cal_z)) {
 		do {
-			v19 = *cal_y - ((((*cal_y <= 0) - 1) & 2) - 1);
-			*cal_y = v19;
-		} while (sub_10003400(*cal_x, v19, *cal_z));
+			*cal_z = *cal_z - ((((*cal_z <= 0) - 1) & 2) - 1);
+		} while ( sub_10003400(0, 0, *cal_z) );
+	}
+
+
+	v13 = ((double)*cal_z * 0.001 + 1.0) * 0.9259259259259258 * (double)(x2_cyan - x1_yellow) * 3.0 * 0.5;
+	v13 = (v13 < 0.0) ? v13 - 0.5 : v13 + 0.5;
+	*cal_x = (int)v13;
+
+	if ( sub_10003400(*cal_x, 0, *cal_z) ) {
+		do {
+			*cal_x = *cal_x - ((((*cal_x <= 0) - 1) & 2) - 1);
+		} while ( sub_10003400(*cal_x, 0, *cal_z) );
+	}
+
+
+	v16 = ((double)*cal_z * 0.001 + 1.0) * 0.9259259259259258 * (double)(y2_green - y1_magenta) * 3.0 * 0.5;
+	v16 = (v16 < 0.0) ? v16 - 0.5 : v16 + 0.5;
+	*cal_y = (int)v16;
+
+	if (sub_10003400(*cal_x, *cal_y, *cal_z)) {
+		do {
+			*cal_y = *cal_y - ((((*cal_y <= 0) - 1) & 2) - 1);
+		} while (sub_10003400(*cal_x, *cal_y, *cal_z));
 	}
 }
 
